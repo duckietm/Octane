c6170bf3740344d7c999083b29ef47e96d7d8fe9
Luca Di Nuzzo <lucadinuzzo22@gmail.com>
2026-09-09 15:17:08 +0200

    refactor(AvatarEditorFigurePreviewView): simplify rotateFigure logic using modulo

--- src/components/avatar-editor/AvatarEditorFigurePreviewView.tsx
+++ src/components/avatar-editor/AvatarEditorFigurePreviewView.tsx
@@ -5,21 +5,14 @@ import { LayoutAvatarImageView } from '../../common';
 import { useAvatarEditor } from '../../hooks';
 
 const DEFAULT_DIRECTION: number = 4;
+const AVATAR_DIRECTIONS: number = 8;
 
 export const AvatarEditorFigurePreviewView: FC<{}> = (props) => {
     const [direction, setDirection] = useState<number>(DEFAULT_DIRECTION);
     const { getFigureString = null, gender = 'M' } = useAvatarEditor();
 
-    const rotateFigure = (newDirection: number) => {
-        if (newDirection < AvatarDirectionAngle.MIN_DIRECTION) {
-            newDirection = AvatarDirectionAngle.MAX_DIRECTION + (newDirection + 1);
-        }
-
-        if (newDirection > AvatarDirectionAngle.MAX_DIRECTION) {
-            newDirection -= AvatarDirectionAngle.MAX_DIRECTION + 1;
-        }
-
-        setDirection(newDirection);
+    const rotateFigure = () => {
+        setDirection(curr => (curr + 1) % AVATAR_DIRECTIONS);
     };
 
     return (
@@ -27,7 +20,7 @@ export const AvatarEditorFigurePreviewView: FC<{}> = (props) => {
             <div className="figure-preview-container">
                 <LayoutAvatarImageView direction={direction} figure={getFigureString} gender={gender} />
             </div>
-            <button type="button" className="octane-avatar-editor-rotate" aria-label="Rotate avatar" onClick={() => rotateFigure(direction + 1)}>
+            <button type="button" className="octane-avatar-editor-rotate" aria-label="Rotate avatar" onClick={rotateFigure}>
                 <img src={rotateSrc} alt="" draggable={false} />
             </button>
         </div>
